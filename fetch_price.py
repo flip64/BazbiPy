fa6fea989bdd348a4d4fa81f1b870fcbe085bb13
flip <jr64.naderloo@gmail.com>
2025-06-03 02:36:33 +0330

    name&price

--- fetch_price.py
+++ fetch_price.py
@@ -15,7 +15,31 @@ def get_price(url):
     price_tag = soup.find("span", class_="woocommerce-Price-amount amount")
     if price_tag:
         price = price_tag.get_text(strip=True)
-        print(f"قیمت قهوه جوش ۳ کاپ استیل: {price}")
+        print(f"  قیمت     : {price}")
     else:
         print("قیمت یافت نشد.")
+ 
+
+
+
+
+def fetch_product_name_price(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # استخراج نام محصول
+    name_tag = soup.find("h1", class_="product_title")
+    product_name = name_tag.text.strip() if name_tag else None
+
+    # استخراج قیمت محصول
+    price_tag = soup.find("span", class_="woocommerce-Price-amount")
+    product_price = price_tag.text.strip() if price_tag else None
+
+    return product_name, product_price
+
 
