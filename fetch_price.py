28432d1e4ba8b872ed0c2e703774137056ee1c73
flip64 <65002468+flip64@users.noreply.github.com>
2025-06-02 19:40:06 +0330

    Update fetch_price.py

--- fetch_price.py
+++ fetch_price.py
@@ -17,8 +17,3 @@ def fetch_price(url):
             return "قیمت پیدا نشد."
     else:
         return f"خطا در دریافت صفحه. کد وضعیت: {response.status_code}"
-
-if __name__ == "__main__":
-    product_url = "https://pakhshabdi.com/product/%da%a9%d8%a7%d8%b1%d9%88%d8%a7%d8%b4-%d8%b3%d8%b1-%d8%a8%d8%b7%d8%b1%db%8c/"
-    price = fetch_price(product_url)
-    print(f"قیمت محصول: {price}")
