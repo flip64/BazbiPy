15385d5b287d98586ec9d1afb758542549016b4a
flip64 <65002468+flip64@users.noreply.github.com>
2025-06-03 14:34:02 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import fetch_price
+import track_product_price
 url = "https://pakhshabdi.com/product/%da%af%db%8c%d8%b1%d9%87-%d9%86%da%af%d9%87%d8%af%d8%a7%d8%b1%d9%86%d8%af%d9%87-%d8%b3%d8%b1%d8%af%d9%88%d8%b4/"
 
 
-
-name, price = fetch_price.fetch_product_name_price(url)
-print(f"نام محصول: {name}")
-print(f"قیمت: {price}")
+track_product_price(url)
